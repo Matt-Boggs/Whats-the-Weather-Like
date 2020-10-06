836a8bdc9794c406f6fb05d93f31b61dd3c3ba03
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-06 16:03:47 -0600

    Tidying up

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,32 +1,3 @@
-
-// EACH SEARCH WILL PUSH TO THIS ARRAY
-// SEARCHES = [
-//     city1 = {
-//         name: "bla",
-//         temp: "bla",
-//         humid: "bla",
-//         wind: "bla",
-//         UV: "bla"
-//     },
-//     city2 = {
-//             name: "bla",
-//             temp: "bla",
-//             humid: "bla",
-//             wind: "bla",
-//             UV: "bla"
-//         }
-// ]
-
-
-// MAYBE CREATE AN ARRAY OF CITIES AS OBJECTS, SO THE ARRAY CAN BE SAVED TO LOCAL STORAGE AND THEN GRABBED FOR THE SEARCH HISTORY
-//##### Unit 06 Homework Summary and Tips #########
-// My tips:
-//   a. Make sure you can retrieve data via the API for a city, both for the current day and a multi-day forecast (these may need to be two separate API calls)
-//   b. Determine how you'll manage the list of cities to the left. When a user enters a city name, after the response is received from OpenWeather, the city name should be added to the list. Be sure to prevent duplicates. You'll need to store the city list in local storage as well, so get that process figured out.
-//   c. When the user clicks on a city name, the API query should run again, just as if the user had typed the city in at the top. So maybe the city-typing and the city-clicking should both go to the same function for API lookup... ? 
-
-
-
 $(document).ready(function(){
     var x=0;
     var searches = [];
@@ -62,7 +33,7 @@ $(document).ready(function(){
 
            var UVData = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
-           //this populates the UV index field, and colors the background according to severity of UV exposure
+           // This populates the UV index field, and colors the background according to severity of UV exposure
            $.ajax({
            url: UVData,
            method: "GET"
@@ -97,20 +68,20 @@ $(document).ready(function(){
             wind: windSpeed,
             UV: UVIdx,
             };
-            searches.push(search)
-            console.log(search)
-            console.log(searches)
+            searches.push(search);
+            console.log(search);
+            console.log(searches);
        });
 
-       //5 day forecast code goes here
+       // 5 day forecast code goes here
        $.ajax({
         url: forecast,
         method: "GET"
         }).then(function(fiveDay) {
-            var cityCast = $("<div>")
-            cityCast.addClass("col-1 card")
-            cityCast.text((city).toUpperCase())
-            $("#forecast").append(cityCast)
+            var cityCast = $("<div>");
+            cityCast.addClass("col-1 card");
+            cityCast.text((city).toUpperCase());
+            $("#forecast").append(cityCast);
             for (i=0;i<40;i+=8){
                 var foreIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay.list[i].weather[0].icon + ".png");
                 
@@ -125,8 +96,8 @@ $(document).ready(function(){
                 var humid = $("<p>").text("Humidity: " + fiveDay.list[i].main.humidity);
                 oneDay.append(date).append(temp).append(humid);
                 $("#forecast").append(oneDay);
-            }
-        })
+            };
+        });
        
 
 
@@ -136,29 +107,12 @@ $(document).ready(function(){
         previousSearch.addClass("card-body prevSearch");
         previousSearch.text(city);
         $("#history").append(previousSearch);
-    }
-    $("#searchBtn").on("click", grabThatCast)
-    var prevSearch = $(".prevSearch")
+    };
+    $("#searchBtn").on("click", grabThatCast);
+    var prevSearch = $(".prevSearch");
     prevSearch.on("click", function(){
-        savedID = $(this).text()
-        console.log(savedID)
+        savedID = $(this).text();
+        console.log(savedID);
 
-    })
-
-
-
-
+    });
 });
-        
-        
-        // previousSearch.on("click", function(){
-        //     console.log("click")
-        //     city = $(this).text()
-        //     console.log(city)
-        //     grabThatCast(city)
-        //     // Where should storage stuff be set?
-        //     // Grab local storage stuff?
-
-
-        
-
